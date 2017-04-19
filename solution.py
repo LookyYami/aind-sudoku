c95a8c1321aa1bd67ecf9f600ff1c1c77d98a9cf
Lucas Silva <Lucas Silva>
2017-04-19 18:35:08 -0300

    First commit

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+from utils import *
+
 assignments = []
 
 def assign_value(values, box, value):
@@ -23,13 +25,30 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
-
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    for unit in unilist:
+        
+    twins = [kBox for kBox in values.keys() if len(values[kBox]) == 2]
+    inverse = {}
+    for kBox in twins:
+        inverse.setdefault(values[kBox], []).append(kBox)
+    for twin in inverse:
+            for box in inverse[twin]:
+                #print(box)
+                for peer in peers[box]:
+                    if peer in inverse[twin]:
+                        #print('peer:', peer)
+                        for digit in twin:
+                            #print('digit:',digit)
+                            #print(peers[box] & peers[peer]  )
+                            for p in peers[box]:
+                                if len(values[p]) > 2:
+                                    values = assign_value(values, p, values[p].replace(digit, ''))
+    
+    return values
+                        
 
 def grid_values(grid):
     """
@@ -41,27 +60,72 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
-
-def display(values):
-    """
-    Display the values as a 2-D grid.
-    Args:
-        values(dict): The sudoku in dictionary form
-    """
-    pass
+    r = dict(zip(boxes, grid))
+    for k, v in r.items():
+        if v == '.':
+            r = assign_value(r, k, '123456789')
+    return r
 
 def eliminate(values):
-    pass
+    for kBox, vBox in values.items():
+        if len(vBox) == 1:
+            for peer in peers[kBox]:
+                values = assign_value(values, peer, values[peer].replace(vBox, ''))
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for d in '123456789':
+            b = ''
+            for box in unit:
+                if d in values[box]:
+                    b = b + box
+            if len(b) == 2:
+                values = assign_value(values, b, d)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Use the Naked Twins Strategy
+        values = naked_twins(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    d = {}
+    for k, v in values.items():
+        if len(v) > 1:
+            d.update({k:v})
+    b = min(d, key=d.get)
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for v in values[b]:
+        d = values.copy()
+        d[b] = v
+        attempt = search(d)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,14 +136,41 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    sudoku = grid_values(grid)
+    return search(sudoku)
+
+before_naked_twins_1 = {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8',
+                            'H5': '6', 'F9': '7', 'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8',
+                            'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23', 'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5',
+                            'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'A4': '2357', 'A7': '27',
+                            'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
+                            'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2',
+                            'F6': '125', 'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '379', 'F1': '6',
+                            'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'E2': '37', 'F7': '35', 'F8': '9',
+                            'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17', 'D3': '2379', 'B4': '27',
+                            'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'D6': '279',
+                            'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
 
 if __name__ == '__main__':
-    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(solve(diag_sudoku_grid))
+    #diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    #display(solve(diag_sudoku_grid))
+    display(before_naked_twins_1)
+    print("\n")
+    display(naked_twins(before_naked_twins_1))
+    print("\n")
+    display({'G7': '6', 'G6': '3', 'G5': '2', 'G4': '9', 'G3': '1', 'G2': '8', 'G1': '7', 'G9': '5', 'G8': '4', 'C9': '1',
+         'C8': '5', 'C3': '8', 'C2': '237', 'C1': '23', 'C7': '9', 'C6': '6', 'C5': '37', 'A4': '2357', 'A9': '8',
+         'A8': '6', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'F6': '125', 'F7': '35', 'F8': '9',
+         'F9': '7', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'C4': '4',
+         'B8': '3', 'B9': '4', 'I9': '9', 'I8': '7', 'I1': '23', 'I3': '23', 'I2': '6', 'I5': '5', 'I4': '8', 'I7': '1',
+         'I6': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'E8': '1', 'A7': '27', 'A6': '257', 'E5': '347',
+         'E4': '6', 'E7': '345', 'E6': '579', 'E1': '8', 'E3': '79', 'E2': '37', 'H8': '2', 'H9': '3', 'H2': '9',
+         'H3': '5', 'H1': '4', 'H6': '17', 'H7': '8', 'H4': '17', 'H5': '6', 'D8': '8', 'D9': '6', 'D6': '279',
+         'D7': '34', 'D4': '237', 'D5': '347', 'D2': '1', 'D3': '79', 'D1': '5'})
 
     try:
-        from visualize import visualize_assignments
-        visualize_assignments(assignments)
+        pass#from visualize import visualize_assignments
+        #visualize_assignments(assignments)
 
     except SystemExit:
         pass
